104447012f3a3c9703682f6be1562e28d2388e47
Tuğçe Kolcu <tugce.kolcu@akbank.com>
2018-01-04 23:30:30 +0300

    Temiz kod

--- app/src/main/java/android/tugcekolcu/firstapplication/TextPlay.java
+++ app/src/main/java/android/tugcekolcu/firstapplication/TextPlay.java
@@ -18,7 +18,7 @@ import java.util.Random;
  * Created by tugcekolcu on 3.01.2018.
  */
 
-public class TextPlay extends Activity {
+public class TextPlay extends Activity implements View.OnClickListener{
 
     EditText etText;
     TextView textOnScreen;
@@ -26,79 +26,78 @@ public class TextPlay extends Activity {
     ToggleButton tbChange;
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.textplay);
 
+        init();
+
+        btnSend.setOnClickListener(this);
+        tbChange.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        int a = v.getId();
+
+        if (v.getId() == R.id.button) {
+
+            String input = etText.getText().toString();
+
+            if (!"".equals(input) || input != null) {
+
+                textOnScreen.setText(input);
+
+                if (input.equals("Left")) {
+                    textOnScreen.setGravity(Gravity.LEFT);
+                } else if (input.equals("Right")) {
+                    textOnScreen.setGravity(Gravity.RIGHT);
+                } else if (input.equals("Center")) {
+                    textOnScreen.setGravity(Gravity.CENTER);
+
+                } else if (input.equals("default")) {
+                    Random rnd = new Random();
+                    textOnScreen.setText("default");
+                    textOnScreen.setTextColor(Color.rgb(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)));
+
+                    switch (rnd.nextInt(3)) {
+                        case 0:
+                            textOnScreen.setGravity(Gravity.LEFT);
+                            break;
+                        case 1:
+                            textOnScreen.setGravity(Gravity.RIGHT);
+                            break;
+                        case 2:
+                            textOnScreen.setGravity(Gravity.CENTER);
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    textOnScreen.setText("invalid");
+                    textOnScreen.setGravity(Gravity.CENTER);
+                }
+
+            }
+        } else if(v.getId()==R.id.button2) {
+            if (tbChange.isChecked()) { //toggle butonu acik ise
+                etText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            } else { //toggle butonu kapali ise
+
+                etText.setInputType(InputType.TYPE_CLASS_TEXT);
+            }
+
+        }
+
+    }
+    public void init(){
+
         etText = (EditText) findViewById(R.id.editText);
-        textOnScreen=(TextView) findViewById(R.id.degisecekyazi);
-        btnSend=(Button) findViewById(R.id.button);
-        tbChange=(ToggleButton) findViewById(R.id.button2);
-
-        tbChange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(tbChange.isChecked()){ //toggle butonu acik ise
-                    etText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD );
-
-                }
-                else{ //toggle butonu kapali ise
-
-                    etText.setInputType(InputType.TYPE_CLASS_TEXT);
-                }
-
-            }
-        });
-
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String input = etText.getText().toString();
-
-                if(!"".equals(input) || input!=null){
-
-                    textOnScreen.setText(input);
-
-                    if(input.equals("Left")){
-                        textOnScreen.setGravity(Gravity.LEFT);
-                    }
-                    else if(input.equals("Right")){
-                        textOnScreen.setGravity(Gravity.RIGHT);
-                    }
-                    else if(input.equals("Center")){
-                        textOnScreen.setGravity(Gravity.CENTER);
-
-                    }
-                    else if(input.equals("tugce")){
-                        Random rnd = new Random();
-                        textOnScreen.setText("tugce");
-                        textOnScreen.setTextColor(Color.rgb(rnd.nextInt(255),rnd.nextInt(255),rnd.nextInt(255)));
-
-                        switch (rnd.nextInt(3)){
-                            case 0:
-                                textOnScreen.setGravity(Gravity.LEFT);
-                                break;
-                            case 1:
-                                textOnScreen.setGravity(Gravity.RIGHT);
-                                break;
-                            case 2:
-                                textOnScreen.setGravity(Gravity.CENTER);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                    else{
-                        textOnScreen.setText("Gecersiz");
-                        textOnScreen.setGravity(Gravity.CENTER);
-                    }
-
-                }
-
-            }
-        });
-
+        textOnScreen = (TextView) findViewById(R.id.degisecekyazi);
+        btnSend = (Button) findViewById(R.id.button);
+        tbChange = (ToggleButton) findViewById(R.id.button2);
     }
 }
